36ea4e55b17ca2e9948eb865c33e740394b54ad9
acc13 <andrew_chang1@hotmail.com>
2015-01-15 20:49:57 -0500

    cleaned up

--- decode.py
+++ decode.py
@@ -4,27 +4,20 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 """
-maps a given alphabet:
-'gP9jOzA75VyIW6FRuHZimQcLB1rCTKpGN8hdEXw0eY3kMx+/JDUntsob4a2qfSvl'
-to the 64 bit table:
-'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-
-ucaUCo4NmA88Tws8BoQsKAXdBcDn
+decode string
 """
 
 import sys
-
 import base64
-coded_string = '''Q5YACgA...'''
-
 
 class Decoder:
-    """decodes string mapping given alphabet to 64 bit encoded chars"""
+    """decode string"""
     tmpfilename = "tmp"
 
     @staticmethod
-    def decode(str, alph1, alph2):
-        """decodes str, mapping alphabet 1 to alphabet 2"""
+    def decode(str, alph1):
+        """64 bit decoding using custom alphabet alph1"""
+        alph2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
         print(str)
         print(alph1)
         print(alph2)
@@ -45,32 +38,11 @@ class Decoder:
     
     
 
-def usage():
-    """print usage"""
-    print('usage: -url url -email email -password password [-noemail]')
-    sys.exit(1)
-    
 def main():
-    """script main method"""
-    
-    '''
-    #configloader tests
-    args = sys.argv[1:]
-
-    if not args or len(args) < 6:
-        usage()
-        
-    if args[0] == '-url':
-        url = args[1]
-        del args[0:2]
-    else:
-        usage()'''
-    
     str = 'ucaUCo4NmA88Tws8BoQsKAXdBcDn'
     alph1 = 'gP9jOzA75VyIW6FRuHZimQcLB1rCTKpGN8hdEXw0eY3kMx+/JDUntsob4a2qfSvl'
-    alph2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
     
-    Decoder.decode(str, alph1, alph2)
+    Decoder.decode(str, alph1)
         
     sys.exit(0)    
         
